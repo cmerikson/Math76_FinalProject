323538c11586229e1032d4662196aac60e9e03c6
cmerikson <cmerikson1@gmail.com>
2024-08-09 18:44:30 -0400

    Added water mask

--- Julia_Segment.jl
+++ Julia_Segment.jl
@@ -1,4 +1,4 @@
-using Images, ImageView, ImageSegmentation, ImageDraw, FileIO, Plots, PyCall, Glob, DataFrames, Dates
+using Images, ImageView, ImageSegmentation, ImageDraw, ImageMorphology, FileIO, Plots, PyCall, Glob, DataFrames, Dates
 
 # Function for calling band selection from python script
 function select_bands(file::String, bandA::Int, bandB::Int, bandC::Union{Nothing, Int}, output::String)
@@ -115,7 +115,7 @@ function segmented_object(file_path::String, Seed1::Tuple{Int64,Int64}, Seed2::T
     end
 end
 
-function segment_mask(folder::String, Seed1::Tuple{Int64,Int64}, Seed2::Tuple{Int64,Int64}; Seed3::Union{Nothing, Tuple{Int64,Int64}} = nothing, Seed4::Union{Nothing, Tuple{Int64,Int64}} = nothing, Display::Bool = false, threshold::Float64 = 1.0, crop_size::Union{Nothing, Tuple{Int, Int}}=nothing, mods::Union{Nothing, Vector{Tuple{Real, Real, Vararg{Float64}}}}=nothing)
+function segment_mask(folder::String, Seed1::Tuple{Int64,Int64}, Seed2::Tuple{Int64,Int64}; Seed3::Union{Nothing, Tuple{Int64,Int64}} = nothing, Seed4::Union{Nothing, Tuple{Int64,Int64}} = nothing, Display::Bool = false, ndvi_threshold::Float64 = 1.0, ndwi_threshold::Float64 = 1.0, crop_size::Union{Nothing, Tuple{Int, Int}}=nothing, mods::Union{Nothing, Vector{Tuple{Real, Real, Vararg{Float64}}}}=nothing)
     
     # Get all files with tif extension
     files = glob("*.tif", folder)
@@ -151,16 +151,26 @@ function segment_mask(folder::String, Seed1::Tuple{Int64,Int64}, Seed2::Tuple{In
     
     for i in [1:1:file_count;]
         rgb_path = select_bands(sorted[i], 1,2,3, joinpath(temp_dir, "rgb.jpg"))
-        ndvi_path = select_bands(sorted[i], 1,4,nothing, joinpath(temp_dir, "ndvi.jpg"))
+        ndvi_path = select_bands(sorted[i], 4,1,nothing, joinpath(temp_dir, "ndvi.jpg"))
 
         year, week = extract_date(sorted[i])
 
         if i == 1
+            if ndwi_threshold < 1
+                ndwi_path = select_bands(sorted[i], 5,4,nothing, joinpath(temp_dir, "ndwi.jpg"))
+                ndwi = load(ndwi_path)
+                binary_ndwi = ndwi .< ndwi_threshold
+                binary_ndwi = dilate(binary_ndwi)
+            else
+                binary_ndwi = 1.0
+            end
+            
             pixel_count = count_pixels(rgb_path, Seed1, Seed2, Seed3=Seed3, Seed4=Seed4, Display=false, crop_size=crop_size, mods=mods)
             row = DataFrame(Year=year, Week=week, Pixels=pixel_count)
             append!(results, row)
             segments = segmented_object(rgb_path, Seed1, Seed2, Seed3=Seed3, crop_size=crop_size, mods=mods)
             mask = labels_map(segments) .== 1
+            mask = mask .* binary_ndwi
 
             if Display
                 masks = masks .+ mask
@@ -187,9 +197,9 @@ function segment_mask(folder::String, Seed1::Tuple{Int64,Int64}, Seed2::Tuple{In
                 # Get NDVI of current image
                 NDVI = load(ndvi_path)
 
-                if threshold < 1.0
+                if ndvi_threshold < 1.0
                     # Binarize
-                    NDVI = (NDVI .< threshold)
+                    NDVI = (NDVI .> ndvi_threshold)
                 end
 
                 if crop_size != nothing
